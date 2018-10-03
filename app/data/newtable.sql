e6c1e58c18eedfcdf2e5a80aa1a12848fc65d27d
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-03 14:17:16 -0400

    Populate new data

---
+++ app/data/newtable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Homework (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  comment VARCHAR(80) NOT NULL,
+);
+
+INSERT INTO Homework (id, comment)
+VALUES (1, 'I have something important to say');
+INSERT INTO Homework (id, comment)
+VALUES (2, 'D&S is awesome');
+INSERT INTO Homework (id, comment)
+VALUES (3, 'Core is harder than I thought');
