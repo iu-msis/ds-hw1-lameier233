a90efd52176d8f38d03bd8462f0506b2a382e0e0
Lana La <42561391+lameier233@users.noreply.github.com>
2018-10-03 14:22:54 -0400

    Setting up php file

---
+++ app/models/Homework.php
@@ -0,0 +1,34 @@
+<?php
+
+class Homework
+{
+  public $id;
+  public $comment;
+
+  public function __construct($data) {
+    $this->id = intval($data['id']);
+    $this->comment = $data['comment'];
+  }
+
+  public static function fetchAll() {
+    // 1. Connect to the database
+    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+    // 2. Prepare the query
+    $sql = 'SELECT * FROM Homework';
+    $statement = $db->prepare($sql);
+
+    // 3. Run the query
+    $success = $statement->execute();
+
+    // 4. Handle the results
+    $arr = [];
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+      $newHomework =  new Homework($row);
+      array_push($arr, $newHomework);
+    }
+
+    return $arr;
+  }
+
+}
